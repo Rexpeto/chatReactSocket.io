0ff3e46e0eae76566ec0b7b10c04f52b6036953f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-14 23:55:25 -0400

    Use React-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import {
+    RiSendPlane2Fill,
+    RiEmotionLaughLine,
+    RiImageFill,
+} from "react-icons/ri";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:5000");
 
 function App() {
     const [message, getMessage] = useState("");
-    
+
     return (
         <div className="container mx-auto px-4 h-screen flex items-center">
             <div className="w-full h-5/6 relative text-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -15,37 +20,13 @@ function App() {
                             type="button"
                             className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 transition-all duration-150"
                         >
-                            <svg
-                                aria-hidden="true"
-                                className="w-6 h-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                                    clip-rule="evenodd"
-                                ></path>
-                            </svg>
+                            <RiImageFill className="w-6 h-6" />
                         </button>
                         <button
                             type="button"
                             className="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 transition-all duration-150"
                         >
-                            <svg
-                                aria-hidden="true"
-                                className="w-6 h-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z"
-                                    clip-rule="evenodd"
-                                ></path>
-                            </svg>
+                            <RiEmotionLaughLine className="w-6 h-6" />
                         </button>
                         <input
                             type="text"
@@ -56,17 +37,9 @@ function App() {
                         ></input>
                         <button
                             type="submit"
-                            className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600 transition-all duration-150"
+                            className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600 transition-all duration-150 group"
                         >
-                            <svg
-                                aria-hidden="true"
-                                className="w-6 h-6 rotate-90"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                            </svg>
+                            <RiSendPlane2Fill className="h-6 w-6 group-hover:rotate-31 transition-all duration-500" />
                         </button>
                     </div>
                 </form>
