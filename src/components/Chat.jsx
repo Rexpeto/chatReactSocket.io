9d5c95ee060a1cdcc4ed760e00457a4d4a6f2423
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-15 17:12:48 -0400

    Use component Message

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,11 @@
-const Chat = ({messages}) => {
+import Message from "./Message";
+
+const Chat = ({ messages }) => {
     return (
         <div className="overflow-y-scroll h-[93%] flex flex-col px-4 gap-3 py-[1.5rem]">
-            <div className="bg-blue-600 flex rounded-lg px-4 max-w-[100px]">
-                <p>Prueba</p>
-            </div>
-            <div className="bg-green-600 flex px-4 max-w-[100px] rounded-lg self-end">
-                <p>Prueba</p>
-            </div>
+            {messages.map((message, index) => (
+                <Message key={index} message={message} />
+            ))}
         </div>
     );
 };
